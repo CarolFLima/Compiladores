05705e5487426ffa01040922f4d3c86bda3daa03
Caroline Lima <ncfl@ic.ufal.br>
2017-03-06 23:24:05 -0300

    adiciona classe Token

--- AnalisadorLexicoKia/src/Token.java
+++ AnalisadorLexicoKia/src/Token.java
@@ -1,4 +1,51 @@
 
 public class Token {
 
+	private Terminals type;
+	private String value;
+	private int row;
+	private int column;
+	
+	public Token(Terminals type, String value, int row, int column){
+		this.type = type;
+		this.value = value;
+		this.row = row;
+		this.column = column;
+	}
+	
+	// **************** Getters and setters ****************
+	public Terminals getType() {
+		return type;
+	}
+	public void setType(Terminals type) {
+		this.type = type;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getColumn() {
+		return column;
+	}
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	// **************** Methods ****************
+	
+	@Override 
+	public String toString(){
+		// TODO
+		return "";
+	}
+	
+	
 }
